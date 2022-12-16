3c95fa6aa7111b47d20d81aa38e353d069f1f5dd
RickyPatel <54434481+RickyPatel@users.noreply.github.com>
2022-12-16 20:29:27 +0530

    first commit

---
+++ client/build/precache-manifest.838470643f5e0cc107670344f3ac247d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8d24b4a53c3325c5f5f",
+    "url": "/static/css/main.08462678.chunk.css"
+  },
+  {
+    "revision": "b8d24b4a53c3325c5f5f",
+    "url": "/static/js/main.b8d24b4a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "9cbccdad4919551ec4e1",
+    "url": "/static/js/2.9cbccdad.chunk.js"
+  },
+  {
+    "revision": "01abdd46bea39d7fe8e4824fb35406e7",
+    "url": "/static/media/books.01abdd46.jpg"
+  },
+  {
+    "revision": "a9ea7397360a3a2a8aadb66555262fce",
+    "url": "/static/media/youwant.a9ea7397.jpg"
+  },
+  {
+    "revision": "3be04660821690de861f039ef6260beb",
+    "url": "/static/media/tired.3be04660.jpg"
+  },
+  {
+    "revision": "c17b9542a480c40e6381a826d0667fb2",
+    "url": "/static/media/cart.c17b9542.png"
+  },
+  {
+    "revision": "abae3096c581937d09f912b18dd19504",
+    "url": "/static/media/product.abae3096.png"
+  },
+  {
+    "revision": "ed390ebbc61b50b7fa7a3d8dfd9ea45b",
+    "url": "/static/media/bg3.ed390ebb.jpg"
+  },
+  {
+    "revision": "a6b6b1ff93560765dc0ca25ad4fbb675",
+    "url": "/index.html"
+  }
+];
