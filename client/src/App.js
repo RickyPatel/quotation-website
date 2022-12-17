17408f6ab52d09da9c204e73e13b2710d4c7956e
RickyPatel <54434481+RickyPatel@users.noreply.github.com>
2022-12-17 13:59:30 +0530

    render deploy changes

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter basename="/">
+        <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/login" component={LogIn} />
